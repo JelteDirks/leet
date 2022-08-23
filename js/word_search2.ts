264b8467e0c35cd629ba926a5fa3979643884f4e
Jelte Dirks <j.dirks@outlook.com>
2022-08-23 14:02:03 +0200

    word search 2 potential solution
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

---
+++ js/word_search2.ts
@@ -0,0 +1,8 @@
+// make adj list
+// loop nodes
+// for every node traverse in bfs and simultaneasly check the position of the
+// strings at depth of the bfs at that time.
+// if no strings have length of pos, continue to the next node and start the
+// traversing there.
+// add the found words to a list. can remove words to search for efficiency,
+// no need to do this to improve running time.
